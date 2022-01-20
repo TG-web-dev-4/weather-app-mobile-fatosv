af4ec32e5244814c0b0d9e167e471ff14abce907
Fatos Varol <ftsvarol@gmail.com>
2022-01-20 21:19:20 +0100

    removed fetch

--- src/screens/WeatherAppScreen.js
+++ src/screens/WeatherAppScreen.js
@@ -7,14 +7,6 @@ const WeatherAppScreen = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState([]);
 
-  // useEffect(() => {
-  //   fetch(
-  //     "https://api.openweathermap.org/data/2.5/weather?q=amsterdam&appid=5897c638224879a677e37baf3ab28b3b&unit=metric"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => setWeather(data));
-  // }, []);
-
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -24,11 +16,8 @@ const WeatherAppScreen = () => {
         <SearchBar
           city={city}
           inputHandler={setCity}
-          onCitySubmit={() => setWeather()}
+          onCitySubmit={() => console.log("city submitted")}
         />
-        <Text>
-          It's {weather.description} in {city}
-        </Text>
         <WeatherCard />
       </ImageBackground>
     </View>
